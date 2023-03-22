b30a1c9263f2aa0b44e30aeb7a5a1e4ea7b44e0d
Nermine Hendy <nermineahendy@ieee.org>
2023-03-22 16:14:41 +1100

    Just annotated

--- AS02_MainProcessSARV2.m
+++ AS02_MainProcessSARV2.m
@@ -1,10 +1,10 @@
-clc; clear; close all
+clc; clear; close all;close all hidden;
 load('Test01.mat')
 %load('matlabOptical.mat')
-close all hidden;
 %% This is a raw-wise FFT / IFFT
 fft1d2 = @ (x) fftshift(fft(fftshift(x,2),[],2),2);
 ifft1d2 = @ (x) ifftshift(ifft(ifftshift(x,2),[],2),2);
+% This is a cloumn-wise FFT - Azimuth
 fft1d1 = @ (x) fftshift(fft(fftshift(x,1),[],1),1);
 ifft1d1 = @ (x) ifftshift(ifft(ifftshift(x,1),[],1),1);
 %% plotting raw time domain signal
@@ -15,25 +15,20 @@ colormap parula
 xlabel('Fast time [\mus]')
 ylabel('Azimuth index')
 title('Step 0: Raw time domain (magnitude)')
+%% STEP6.SAR Image Processing
 %% Step 1: Range Compression
-So = fft1d2(sqd); % FFT the time domain signal (FFT along each eta row)
-% This is the template for the Match Filter
+So = fft1d2(sqd);                                                           % FFT for the time domain raw signal (FFT along each eta row)
+% This is the template for the Matched Filter
 tau = 0;
 sb = exp(1j*pi *   (-2*RadPar.fo * tau + RadPar.K*(FastTime-tau).^2   )    ) ...
     .*(FastTime>(-RadPar.T/2+tau)).*(FastTime<(RadPar.T/2+tau));
 G = conj(fftshift(fft(fftshift(sb))));
-Src  = repmat(G,size(So,1),1).*So; %Equation 5.5
-src  = ifft1d2(Src); %Equation 5.6
+Src  = repmat(G,size(So,1),1).*So;                                          % Equation 5.5 - Multiplying the filter by So (Frequency Domain Multiplication)
+src  = ifft1d2(Src);                                                        % Equation 5.6 - Inverse Fourier transform along each pulse to put the data back into range
 %% Generate the reference signal based on the ground referene point
-So = fft1d2(sqd_ref); % FFT the time domain signal (FFT along each eta row)
-% This is the template for the Match Filter
-tau = 0;
-sb = exp(1j*pi *   (-2*RadPar.fo * tau + RadPar.K*(FastTime-tau).^2   )    ) ...
-    .*(FastTime>(-RadPar.T/2+tau)).*(FastTime<(RadPar.T/2+tau));
-G = conj(fftshift(fft(fftshift(sb))));
-Src  = repmat(G,size(So,1),1).*So; %Equation 5.5
-src_ref  = ifft1d2(Src); %Equation 5.6
-
+So_ref = fft1d2(sqd_ref);                                                   % FFT for the time domain reference signal (FFT along each eta row)
+Src_ref = repmat(G,size(So_ref,1),1).*So_ref;                               % Equation 5.5 - Multiplying the filter by So (Frequency Domain Multiplication)
+src_ref = ifft1d2(Src_ref);                                                 % Equation 5.6 - Inverse Fourier transform along each pulse to put the data back into range
 %% Plotting the range-compressed image
 subplot(2,4,2)
 pc =pcolor(FastTime/1e-6,1:etaTotal,real(src));
@@ -43,15 +38,15 @@ ylabel('Azimuth index')
 title('Step 1: Range compression')
 drawnow
 %% Step 3 Range cell migration compensation
-DeltaR = R - Ro; % Based on the GRP
+DeltaR = R - Ro;                                                            % Based on the GRP
 subplot(2,4,4)
 plot(1:etaTotal,abs(DeltaR));
 xlabel('Azimuth index')
 ylabel('Range compensation [m]')
 title('Step 3.1: Range compensation profile')
 
-% shiftring range cells
-RangeBin = RadPar.ts*c/2;
+% Shifting range cells
+RangeBin = RadPar.ts*c/2;                                                   % Ground range resolution
 NbinsShift = -round(DeltaR/RangeBin);
 for ctr=1:size(src,1)
     src(ctr,:) = circshift(src(ctr,:),NbinsShift(ctr));
@@ -76,26 +71,26 @@ pc.LineStyle='none';
 xlabel('Fast time [ms]')
 ylabel('Azimuth index')
 title('Step 2: Az FFT')
-Param.ts = Param.dt
-
+Param.ts = Param.dt;
 %% Step 4/5 Azimuth compression
-Haz = exp(1j*pi*R*4*RadPar.fo/c);
+%%% Analytical Matched Filter %%%%
+Haz = exp(1j*pi*R*4*RadPar.fo/c);                                           % Azimuth Matched Filter
+%S3 = S2 .* repmat(Haz,1,size(S2,2));
 subplot(2,4,6)
 plot(real(Haz))
 
-%S3 = S2 .* repmat(Haz,1,size(S2,2));
+%%% Empirical Method %%%%
 midpoint = round(size(S2_ref,2)/2)+1;
 S2_ref = repmat(S2_ref(:,midpoint),1,size(S2,2));
 S3 = S2 .* conj(S2_ref);
-sSLC = ifft1d1(S3);
-
-
+%% Step 5 Azimuth IFFT
+sSLC = ifft1d1(S3);                          % Final Focused SAR Image
+%% Plot Focused SAR Image
 figure(2)
 Range =(-(numel(FastTime)/2)*RangeBin:RangeBin:(numel(FastTime)/2-1)*RangeBin);
 speed= mean(sqrt(sum((diff(SatECI,[],2)).^2)) /Param.ts);
-
 CrossRange = (1:etaTotal)*Param.ts*speed/1000;
-%%
+
 sSLC=sSLC./max(abs(sSLC),[],"all");
 ax=gca;
 pc =pcolor(Range/1000,CrossRange,(abs(sSLC)));
