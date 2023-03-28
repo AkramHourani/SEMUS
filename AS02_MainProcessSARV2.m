d9423f61af5811734e08116d0166038aad4f1700
Akram Al-Hourani <akram.hourani@rmit.edu.au>
2023-03-28 14:04:02 +1100

    Update AS02_MainProcessSARV2.m

--- AS02_MainProcessSARV2.m
+++ AS02_MainProcessSARV2.m
@@ -1,12 +1,9 @@
 clc; clear; close all
+load('SAR_Image.mat')
 close all hidden;
-%load('Test01')
-%load('Test02')
-load('SAR_Image')
 %% This is a raw-wise FFT / IFFT
 fft1d2 = @ (x) fftshift(fft(fftshift(x,2),[],2),2);
 ifft1d2 = @ (x) ifftshift(ifft(ifftshift(x,2),[],2),2);
-% This is a cloumn-wise FFT - Azimuth
 fft1d1 = @ (x) fftshift(fft(fftshift(x,1),[],1),1);
 ifft1d1 = @ (x) ifftshift(ifft(ifftshift(x,1),[],1),1);
 %% plotting raw time domain signal
@@ -17,25 +14,29 @@ colormap parula
 xlabel('Fast time [\mus]')
 ylabel('Azimuth index')
 title('Step 0: Raw time domain (magnitude)')
+<<<<<<< Updated upstream
 %% STEP5.SAR Image Processing
+=======
+>>>>>>> Stashed changes
 %% Step 1: Range Compression
-So = fft1d2(sqd);                                   % FFT the time domain signal (FFT along each eta row)
+So = fft1d2(sqd); % FFT the time domain signal (FFT along each eta row)
 % This is the template for the Match Filter
 tau = 0;
 sb = exp(1j*pi *   (-2*RadPar.fo * tau + RadPar.K*(FastTime-tau).^2   )    ) ...
     .*(FastTime>(-RadPar.T/2+tau)).*(FastTime<(RadPar.T/2+tau));
 G = conj(fftshift(fft(fftshift(sb))));
-Src  = repmat(G,size(So,1),1).*So;               % Equation 5.5 - Multiplying the filter by So (Frequency Domain Multiplication)
-src  = ifft1d2(Src);                             % Equation 5.6 - Inverse Fourier transform along each pulse to put the data back into range
+Src  = repmat(G,size(So,1),1).*So; %Equation 5.5
+src  = ifft1d2(Src); %Equation 5.6
 %% Generate the reference signal based on the ground referene point
-So = fft1d2(sqd_ref);                            % FFT the time domain reference signal (FFT along each eta row)
+So = fft1d2(sqd_ref); % FFT the time domain signal (FFT along each eta row)
 % This is the template for the Match Filter
 tau = 0;
 sb = exp(1j*pi *   (-2*RadPar.fo * tau + RadPar.K*(FastTime-tau).^2   )    ) ...
     .*(FastTime>(-RadPar.T/2+tau)).*(FastTime<(RadPar.T/2+tau));
 G = conj(fftshift(fft(fftshift(sb))));
-Src_ref  = repmat(G,size(So,1),1).*So;          % Equation 5.5 - Multiplying the filter by So (Frequency Domain Multiplication)
-src_ref  = ifft1d2(Src_ref);                    % Equation 5.6 - Inverse Fourier transform along each pulse to put the data back into range
+Src  = repmat(G,size(So,1),1).*So; %Equation 5.5
+src_ref  = ifft1d2(Src); %Equation 5.6
+
 %% Plotting the range-compressed image
 subplot(2,4,2)
 pc =pcolor(FastTime/1e-6,1:etaTotal,real(src));
@@ -53,6 +54,8 @@ pc.LineStyle='none';
 xlabel('Fast time [ms]')
 ylabel('Azimuth index')
 title('Step 2: Az FFT')
+
+
 %% Step 3 Range cell migration corretion
 DeltaR = R- Ro; % Based on the GRP
 subplot(2,4,4)
@@ -61,12 +64,12 @@ xlabel('Azimuth index')
 ylabel('Range compensation [m]')
 title('Step 3.1: Range compensation profile')
 
-% shifting the range cells
-RangeBin = RadPar.ts*c; % This is the time-equivalent distance (twice the slant distance)
+% shiftring range cells
+RangeBin = RadPar.ts*c;
 NbinsShift = -round(DeltaR/RangeBin);
-for eta=1:etaTotal
-S2(eta,:) = circshift(S2(eta,:),NbinsShift(eta));
-S2_ref(eta,:) = circshift(S2_ref(eta,:),NbinsShift);
+for AzCtr=1:etaTotal
+    S2(AzCtr,:) = circshift(S2(AzCtr,:),NbinsShift(AzCtr));
+    S2_ref(AzCtr,:) = circshift(S2_ref(AzCtr,:),NbinsShift);
 end
 
 subplot(2,4,3)
@@ -76,42 +79,115 @@ xlabel('Fast time [\mus]')
 ylabel('Azimuth index')
 title('Step 3.2: RCMC')
 drawnow
+<<<<<<< Updated upstream
 %% Step 4 Azimuth compression
 Haz = exp(1j*pi*R*4*RadPar.fo/c);               % Azimuth Analytical Matched Filter
 %S3 = S2 .* repmat(Haz,1,size(S2,2));
+=======
+
+%% Step 4/5 Azimuth compression
+Haz = exp(1j*pi*R*4*RadPar.fo/c);
+>>>>>>> Stashed changes
 subplot(2,4,6)
 plot(real(Haz))
 
-%%% Empirical Method %%%%
+%S3 = S2 .* repmat(Haz,1,size(S2,2));
 midpoint = round(size(S2_ref,2)/2)+1;
 S2_ref = repmat(S2_ref(:,midpoint),1,size(S2,2));
 S3 = S2 .* conj(S2_ref);
+<<<<<<< Updated upstream
 %% Step 5 Azimuth IFFT
 sSLC = ifft1d1(S3);                             % Final Focused SAR Image
 %% plotting (this is an approzimate projection of the swath)
+=======
+sSLC = ifft1d1(S3);
+
+%%
+>>>>>>> Stashed changes
 figure(2)
+clf
+%speed= mean(sqrt(sum((diff(SatECI,[],2)).^2)) /Param.ts);
+%CrossRange = (1:etaTotal)*Param.ts*speed/1000;
+Img=abs(sSLC)./max(abs(sSLC),[],"all");
+Calibration = 1;
 
-% This is the time-equivalent range (i.e. twice the slant range)
-Range =(-(numel(FastTime)/2)*RangeBin:RangeBin:(numel(FastTime)/2-1)*RangeBin);
-[~,El,~]= geodetic2aer(GRP(1),GRP(2),0,Satlla(1,1),Satlla(1,2),Satlla(1,3),E);
-
-RangeGround = Range/2/cosd(El);
-
-speed= mean(sqrt(sum((diff(SatECI,[],2)).^2)) /Param.ts);
-CrossRange = (1:etaTotal)*Param.ts*speed/1000;
-
-J = abs(sSLC);
-J = J./max(J,[],"all");
-J = imresize(J,2);
-J  = imgaussfilt(J ,2); % Smothing filter
-J = imadjust(J,[0 0.6]);
-imagesc(RangeGround/1000,CrossRange,J)
+% Time equivalent range (i.e. twice the slant range in case of mono-staitic SAR
+RangeEq =(-(numel(FastTime)/2+Calibration)*RangeBin:RangeBin:(numel(FastTime)/2-Calibration-1)*RangeBin);
 ax=gca;
+pc =pcolor(RangeEq/1000,1:size(Img,1),Img);
+pc.LineStyle='none';
 ax.YAxis.Direction = 'reverse';
 ax.XAxis.Direction = 'reverse';
-xlabel('Ground distance (approx.) [km]')
+xlabel('Slant Range [km]')
 ylabel('Cross Range [km]')
 title('Step 5: Comressed image')
-colormap bone
+colormap turbo
+%axis equal
+drawnow
+%imwrite(abs(sSLC),"Mono_SAR.png");
+% Geographic projection
+
+% First: Creat transformation control points in Lat/Lon domain
+
+ResAz = 10;
+ResR  = 10;
+if etaTotal> ResAz
+    n = round(etaTotal/ResAz);
+    etaVec = downsample(1:etaTotal,n); % Take a subsample from the slow time vector
+    ResAz = length(etaVec);
+else 
+    ResAz=etaTotal;
+end
+
+for AzCtr =1:ResAz % Creat the points based on the swath
+    eta =etaVec(AzCtr);
+    % build the conneting line between the swath points
+    [CLat(AzCtr,:),CLon(AzCtr,:)] = gcwaypts(latSwathL1(eta),lonSwathL1(eta),latSwathL2(eta),lonSwathL2(eta),ResR-1);
+
+end
+
+% Second: claculate the transformation control points in Az/Range domain based on given Lat/Lon
+for Ctr=1:length(CLat(:))
+    % Find the disance profile of the target over the entire aquisition period
+    [~,~,RTemp]=geodetic2aer(CLat(Ctr),CLon(Ctr),0,Satlla(:,1),Satlla(:,2),Satlla(:,3),E);
+    [row,col] = ind2sub(size(CLat),Ctr); % Find the row column indices of the control point
+    % find the closest approach
+    
+    [Rmin, etaMin] = min(RTemp); 
+    ContAz(row,col) = etaMin; % This is the slow-time of the closest approach
+    ContR(row,col)  = (Rmin-Ro)*2; % This is the SAR distance to the control point
+end
+
+% Third: Create the inverse transmofation function Az/Range -> Lat/Lon
+AzR2LatLon = fitgeotform2d([CLat(:),CLon(:)],[ContAz(:),ContR(:)],"polynomial",4);
+LatLon2AzR = fitgeotform2d([ContAz(:),ContR(:)],[CLat(:),CLon(:)],"polynomial",4);
+
+% Test the tranformation
+Output1 = transformPointsInverse(LatLon2AzR,[Targetlat(:), Targetlon(:)]);
+hold on
+%scatter(Output1(:,2)/1000,Output(:,1),"+","MarkerEdgeColor",ax.ColorOrder(2,:))
+%% 
+% Transform to local cartisian coordiantes
+figure(3)
+clf
+% Transform from AzR -> Lat/Lon
+etaVecM = repmat((1:etaTotal)',1,length(RangeEq));
+RangeM  = repmat(RangeEq,etaTotal,1);
+[LatImg, LonImg] = transformPointsInverse(AzR2LatLon,etaVecM, RangeM);
+
+% Transfrom from Lat/Lon to NEC
+[xImg,yImg,~]     = latlon2local(LatImg,LonImg,0,GRP);
+%scatter(xImg(:),yImg(:),"+","MarkerEdgeColor",ax.ColorOrder(2,:))
+hold on
 axis equal
-xlim([-1 1]*Swathwidth/2/1000);
+pc =pcolor(xImg/1000,yImg/1000,Img);
+%scatter(xEast(:)/1000,yNorth(:)/1000,"o","MarkerEdgeColor",ax.ColorOrder(2,:))
+scatter(0,0,"+","MarkerEdgeColor",ax.ColorOrder(1,:))
+pc.LineStyle='none';
+grid on
+colormap turbo
+ax.YAxis.Direction = 'reverse';
+ax.XAxis.Direction = 'reverse';
+xlabel('North-axis [km]')
+ylabel('East axis [km]')
+title('Corrected geo image')
