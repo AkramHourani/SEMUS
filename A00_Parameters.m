56f1c14cc3af162c01c7dfdda836081d9fe6411a
Nermine Hendy <124648039+Nermine-Hendy@users.noreply.github.com>
2023-12-01 10:36:29 +1100

    Update A00_Parameters.m

--- A00_Parameters.m
+++ A00_Parameters.m
@@ -25,17 +25,23 @@ stopTime  = startTime + Param.ScanDuration ;    % [s] Set up the end time
 %% Radar Paramters
 RadPar.fo = (500e6);                            % [Hz] Carrier frequency
 RadPar.Lambda = freq2wavelen(RadPar.fo);        % [m] Wavelength
-RadPar.AntOffNadir = 30;                        % [deg] Antenna Off-Nadir angle (pointing angle)
-
-RadPar.BeamRange = 5;                           % [deg] Antenna beamwidth in the range direction
-RadPar.BeamAz    = 5;                           % [deg] Antenna beamwidth in the azimuth direction
-RadPar.Gain  = 10^(12/10);                      % [dBi] Antenna gain example
-
-RadPar.bw = 15e6;                               % [Hz] Bandwidth of the RF signal to calcualte the ramp rate
+RadPar.bw = 20e6;                               % [Hz] Bandwidth of the RF signal to calcualte the ramp rate
 RadPar.fs = RadPar.bw*2;                        % [Hz] Sampling rate (the multiplier is added to improve image quality)
 RadPar.ts = (1/RadPar.fs);                      % [s] Sample time
 RadPar.T = 5e-6;                                % [s] Pulse width
-RadPar.K =  (RadPar.bw /RadPar.T);              % [Hz/s] Ramp (chirp) rate
-
+RadPar.K = (RadPar.bw /RadPar.T);               % [Hz/s] Ramp (chirp) rate
+RadPar.AntOffNadir = 35;                        % [deg] Antenna Off-Nadir angle (pointing angle)
+RadPar.BeamRange = 4;                           % [deg] Antenna beamwidth in the range direction
+RadPar.BeamAz  = 0.2;                           % [deg] Antenna beamwidth in the azimuth direction
 RadPar.SwathWidthDeg = 0.5;                     % [deg] Swath width in degrees
 RadPar.Left = 1;                                % The scanning on the left side of the satellite trajectory
+%% Power Parameters
+RadPar.Gain  = 10^(12/10);                      % [12 dBi] Antenna max gain example in linear scale
+RadPar.Pt = 10^(35/10);                         % [35 dBW] Transmitted power example in Watts
+RadPar.NESO = -24.5;                            % Noise equivalent sigma naught [dB] 
+RadPar.FeederL = 10;                            % Feeder loss [dB]
+RadPar.Duty = 0.1;                              % Duty Cycle
+RadPar.kb = 1.38e-23;                           % Boltzmann's constant [J K−1]
+RadPar.AT = 290;                                 % Absolute temperature [degrees Kelvin]
+RadPar.Bn = Param.PRF/RadPar.Duty;              % Noise equivalent bandwidth [Hz]
+RadPar.F = 8 ;                                  % Noise figure [dB]
