26d8177133f64210a7b032c6e1e4df0035c6130d
Nermine Hendy <124648039+Nermine-Hendy@users.noreply.github.com>
2023-12-01 11:07:40 +1100

    Update NI02a_LoRaGain.m

--- NI02a_LoRaGain.m
+++ NI02a_LoRaGain.m
@@ -9,20 +9,17 @@ zeta = 0.886;
 % Recieved power at SAR antenna from LoRa Tx
 RxGainLORA = single(RadPar.Gain * (sinc(OffBoreSightRLORA*zeta/RadPar.BeamRange)).^2 ...
     .* (sinc(OffBoreSightAzLORA*zeta/RadPar.BeamAz)).^2);
-% imagesc(OffBoreSightRLORA(:),OffBoreSightAzLORA(:),RxGainLORA)
-Interf_ind = find(RxGainLORA >= 1);
+% figure,imagesc(abs(RxGainLORA))
 
-% % Received Power from the signal
-PrdB = 10*log10(sum((abs(sqd)).^2,'all')/size(sqd,1));                   % Received power [dB] using reference signal from all the dwell at GRP
+%% Received Power from the signal
+PrdB = 20*log10(rms(sqd,2));                    % Received power [dB] using reference signal from all the dwell at GRP
 % % For a required SIR
 PLORA_RxRef = PrdB - LORA.SIR;                                                % The desired reference value of LoRa Power in dBm - considered at the min distance
 LoRa_DistRef = min(slantRangeLORA);
-% LoRa_DistRef =slantRangeLORA;
 % LORAPower is adjustable  according to the desired SIR
-LORAPower = 10^(PLORA_RxRef/20) .* fspl(LoRa_DistRef,RadPar.Lambda) ./ LORA.Gain * max(sqrt(RxGainLORA)); 
+LORAPower = 10^(PLORA_RxRef/10) .* 10^(fspl(LoRa_DistRef,RadPar.Lambda)/10) ./ (LORA.Gain * RxGainLORA); 
 % % Convert to dB
-LORAPowerdB = 20*log10(LORAPower);
+LORAPowerdB = 10*log10(LORAPower);
 % Received power at SAR radar from LORA tx in watts
-PLORA = LORAPower * LORA.Gain * sqrt(RxGainLORA)  ./ fspl(slantRangeLORA,RadPar.Lambda);
-% PLORA =flip(PLORA);
-% imagesc(abs(PLORA))
+PLORA = LORAPower * LORA.Gain * RxGainLORA  ./ 10^(fspl(slantRangeLORA,RadPar.Lambda)/10);
+% figure,imagesc(abs(PLORA))
