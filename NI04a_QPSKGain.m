e64e8316502f484dd029cdf8575e66c8541e1f10
Nermine Hendy <nermineahendy@ieee.org>
2023-12-01 11:04:16 +1100

    Updates

--- NI04a_QPSKGain.m
+++ NI04a_QPSKGain.m
@@ -9,14 +9,14 @@ OffBoreSightAzQPSK = azQPSK - sataz;
 RxGainQPSK = single(RadPar.Gain * (sinc(OffBoreSightRQPSK*zeta/RadPar.BeamRange)).^2 ...
     .* (sinc(OffBoreSightAzQPSK*zeta/RadPar.BeamAz)).^2);
 
-% % Received Power from the signal
-PrdB = 10*log10(sum((abs(sqd)).^2,'all')/size(sqd,1));    % Received power [dB] using reference signal from all the dwell at GRP
+%% Received Power from the signal
+PrdB = 20*log10(rms(sqd,2));                    % Received power [dB] using reference signal from all the dwell at GRP
 % % For a required SIR
 PQPSK_RxRef = PrdB - QPSK.SIR;                            % The desired reference value of QPSK Power in dBm - considered at the min distance
 QPSK_DistRef = min(slantRangeQPSK);
 % QPSKPower is adjustable  according to the desired SIR
-QPSKPower = 10^(PQPSK_RxRef/10) .* fspl(QPSK_DistRef,RadPar.Lambda) ./ QPSK.Gain * max(sqrt(RxGainQPSK)); 
+QPSKPower = 10^(PQPSK_RxRef/10) .* 10^(fspl(QPSK_DistRef,RadPar.Lambda)/10) ./ (QPSK.Gain * RxGainQPSK); 
 % % Convert to dB
 QPSKPowerdB = 10*log10(QPSKPower);
 % Received power at SAR radar from QPSK tx in watts
-PQPSK = QPSKPower * QPSK.Gain * sqrt(RxGainQPSK)  ./ fspl(slantRangeQPSK,RadPar.Lambda);
+PQPSK = QPSKPower * QPSK.Gain * RxGainQPSK  ./ 10^(fspl(slantRangeQPSK,RadPar.Lambda)/10);
