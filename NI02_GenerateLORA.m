5103f8451c02d8de246b1ee2befbf36f742ea3e8
Nermine Hendy <124648039+Nermine-Hendy@users.noreply.github.com>
2024-03-19 12:32:52 +0100

    Update NI02_GenerateLORA.m

--- NI02_GenerateLORA.m
+++ NI02_GenerateLORA.m
@@ -19,13 +19,28 @@ sections = round(length(LoRaIQ) / (Discard + Keep));
 for i = 1: sections-1
   slora(i,:) = LoRaIQ(i+(i*Discard)+((i-1)*Keep):i+(i*Discard)+(i*Keep));
 end
-sLORA1 = zeros(1500,size(sqd,2));
-sLORA2 = zeros(345,size(sqd,2));
-sLORA = cat(1,sLORA1,slora,sLORA2);
+% speed= mean(sqrt(sum((diff(SatECI,[],2)).^2)) /Param.tg);       % Platform speed = sqrt(Param.mu/(h+Re))
+Azimuthindex = linspace(-etaTotal*Param.tg*speed/2,etaTotal*Param.tg*speed/2,etaTotal);
+[LORAImgx, LORAImgy, ~] = latlon2local(latLORA,lonLORA,0,GRP);
 
-sLORA = sLORA .* sqrt(PLORA);
-phase_LoRa = exp(-1i*2*pi*RadPar.Lambda/slantRangeLORA);
-sLORA = sLORA .* repmat(phase_LoRa.',1,size(sLORA,2));
+[~, LoRa_azimuth_index] = min(abs(Azimuthindex(:)-LORAImgy));     % linear index of closest entry
+LoRa_azimuth = ind2sub(size(Azimuthindex), LoRa_azimuth_index);   %// convert linear index to row and col
+
+% Index of one end of the LoRa from the interferer tx location (LoRa_azimuth/2)
+Index_before = LoRa_azimuth - floor(size(slora,1)/2);
+% Index of second end of the LoRa from the interferer tx location (LoRa_azimuth/2)
+Index_after = LoRa_azimuth + floor(size(slora,1)/2);
+
+sLORA1 = zeros(Index_before,size(sqd,2));
+sLORA2 = zeros(size(sqd,1)-Index_after,size(sqd,2));
+sLora = cat(1,sLORA1,slora,sLORA2);
+sLora = sLora(1:size(sqd,1),:);
+% figure,imagesc(abs(sLora).^2)
+sLORA = sLora .* sqrt(PLORA);
+%% Adjust Interference phase
+% phase_LoRa = exp(-1i*2*pi*RadPar.Lambda/slantRangeLORA);
+% sLORA = sLORA .* repmat(phase_LoRa.',1,size(sLORA,2));
+%% Plotting
+% figure,imagesc(abs(sLORA).^2)
 % rr= sLORA.';
 % spectrogram(rr(:),500,0,500,RadPar.fs ,'yaxis','centered')
-% imagesc(abs(sLORA).^2)
