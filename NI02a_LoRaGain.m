89a41830df9fd4a79e3cac824e6bc90cbc6a56f8
Nermine Hendy <124648039+Nermine-Hendy@users.noreply.github.com>
2024-03-19 12:34:42 +0100

    Update NI02a_LoRaGain.m

--- NI02a_LoRaGain.m
+++ NI02a_LoRaGain.m
@@ -4,6 +4,11 @@ lonLORA = GRP(2) + LORA.lonShift;                         % LoRa Tx longitude sh
 [azLORA,elevLORA,slantRangeLORA] = geodetic2aer(latLORA,lonLORA,0,Satlla(:,1),Satlla(:,2),Satlla(:,3),E);
 OffBoreSightRLORA = elevLORA + 90 - RadPar.AntOffNadir;
 OffBoreSightAzLORA = azLORA - sataz;
+if RadPar.Left == 0 % RadPar.Left == 0 for the case from South to North - RadPar.Left == 1 for the case from North to South 
+    OffBoreSightAzIR = azInterf - sataz + 360;
+    else
+    OffBoreSightAzIR = azInterf - sataz;
+end
 % The zeta is added such that half the power is matching the beamwidth
 zeta = 0.886;             
 % Recieved power at SAR antenna from LoRa Tx
@@ -12,14 +17,14 @@ RxGainLORA = single(RadPar.Gain * (sinc(OffBoreSightRLORA*zeta/RadPar.BeamRange)
 % figure,imagesc(abs(RxGainLORA))
 
 %% Received Power from the signal
-PrdB = 20*log10(rms(sqd,2));                    % Received power [dB] using reference signal from all the dwell at GRP
+PrdB = 10*log10(sum((abs(sqd)).^2,'all')/size(sqd,1));                   % Received power [dB] using reference signal from all the dwell at GRP
 % % For a required SIR
 PLORA_RxRef = PrdB - LORA.SIR;                                                % The desired reference value of LoRa Power in dBm - considered at the min distance
 LoRa_DistRef = min(slantRangeLORA);
 % LORAPower is adjustable  according to the desired SIR
-LORAPower = 10^(PLORA_RxRef/10) .* 10^(fspl(LoRa_DistRef,RadPar.Lambda)/10) ./ (LORA.Gain * RxGainLORA); 
+LORAPower = 10^(PLORA_RxRef/20) .* fspl(LoRa_DistRef,RadPar.Lambda) ./ LORA.Gain * max(sqrt(RxGainLORA)); 
 % % Convert to dB
-LORAPowerdB = 10*log10(LORAPower);
+LORAPowerdB = 20*log10(LORAPower);
 % Received power at SAR radar from LORA tx in watts
-PLORA = LORAPower * LORA.Gain * RxGainLORA  ./ 10^(fspl(slantRangeLORA,RadPar.Lambda)/10);
+PLORA = LORAPower * LORA.Gain * mean(sqrt(RxGainLORA))  ./ fspl(slantRangeLORA,RadPar.Lambda);
 % figure,imagesc(abs(PLORA))
